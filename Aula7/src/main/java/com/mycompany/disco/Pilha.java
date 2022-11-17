f20807925043e161c6b45aa4e9fc2426b1968922
Samuel Toledo <102129312+TIOSAMBR@users.noreply.github.com>
2022-11-16 22:10:06 -0300

    Aula7
    Aula7

---
+++ Aula7/src/main/java/com/mycompany/disco/Pilha.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.disco;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Samuel Toledo
+ */
+public class Pilha {
+
+    Stack <Integer> pilhaOriginal = new Stack<>(); 
+    Stack <Integer> pilhaDest = new Stack<>(); 
+    Stack <Integer> pilhaAux = new Stack<>(); 
+
+    Long jogadas = (long) 0;
+
+    void torreHanoi(int nDiscos, Stack<Integer> original, Stack <Integer> dest, Stack<Integer> aux){
+
+        if(nDiscos >= 0){
+            torreHanoi(nDiscos -1, original, aux, dest);
+            dest.push(original.pop());
+            jogadas++;
+            // System.out.println("-----");
+            // System.out.println("Original: " + original);
+            // System.out.println("Destino: " + dest);
+            // System.out.println("Auxiliar: " + aux);
+            torreHanoi(nDiscos -1, aux, dest, original);
+        }
+    }
+
+
+}
